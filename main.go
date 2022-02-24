b2ad4f2d5baddb6f864ec6bb66ff2bda1289b631
shivanshtalwar0 <shivanshtalwar0@gmail.com>
2022-02-24 17:15:01 +0530

    barebone websocket communication

--- main.go
+++ main.go
@@ -1,8 +1,74 @@
 package main
+
 import (
+	"flag"
 	"fmt"
-	"github.com/januscaler/networking"
+	"log"
+	"net/http"
+	"os"
+	"github.com/gorilla/websocket"
 )
+
+var addr = flag.String("addr", "0.0.0.0:8080", "http service address")
+var mode = flag.String("mode", "server", "starting as server")
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+        return true
+    },
+} 
+type onMessageCallBack=func(msg string)
+type Connection struct{
+	response http.ResponseWriter
+	request *http.Request
+	conn *websocket.Conn
+	onMessage onMessageCallBack
+	close chan bool
+}
+
+func handleClient(connection *Connection){
+	var err error
+	connection.conn,err=upgrader.Upgrade(connection.response,connection.request,nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer connection.conn.Close()
+	for {
+		_, message, err := connection.conn.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		connection.onMessage(string(message))
+		closeIt:=<-connection.close
+		if(closeIt){
+			break;
+		}
+		// err = c.WriteMessage(1, message)
+		// if err != nil {
+		// 	log.Println("write:", err)
+		// 	break
+		// }
+	}
+
+}
+var connections map[string]*Connection= make(map[string]*Connection)
+func home(response http.ResponseWriter,request *http.Request){
+	connections[request.RemoteAddr]=&Connection{request: request,
+		close: make(chan bool),
+		response: response,onMessage:func(msg string) {
+		fmt.Println(msg)
+		connections[request.RemoteAddr].conn.WriteMessage(1,[]byte(msg))
+
+	}};
+	go handleClient(connections[request.RemoteAddr])
+	<-connections[request.RemoteAddr].close
+}
 func main()  {
-	TestIt()
+	// http.HandleFunc("/echo", echo)
+	flag.NewFlagSet("foo", flag.ExitOnError)
+	fmt.Println(os.Args[1]);
+	fmt.Println(*mode)
+	http.HandleFunc("/websocket", home)
+	log.Fatal(http.ListenAndServe(*addr, nil))
 }
